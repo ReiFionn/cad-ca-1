20561b6cca6e046b829ee0eec719d6145be37ec5
Fionn Reilly <20101977@mail.wit.ie>
2025-11-11 18:51:14 +0000

    modified awards endpoint to work with query parameters, changed some seeding data + some cleanup

--- lib/constructs/auth-api.ts
+++ lib/constructs/auth-api.ts
@@ -31,14 +31,7 @@ export class AuthApi extends Construct {
     this.auth = api.root.addResource("auth");
 
     this.addAuthRoute("signup", "POST", "SignupFn", "signup.ts");
-
-    this.addAuthRoute(
-      "confirm_signup",
-      "POST",
-      "ConfirmFn",
-      "confirm-signup.ts"
-    );
-
+    this.addAuthRoute("confirm_signup", "POST", "ConfirmFn", "confirm-signup.ts");
     this.addAuthRoute("signout", "GET", "SignoutFn", "signout.ts");
     this.addAuthRoute("signin", "POST", "SigninFn", "signin.ts");
   }
